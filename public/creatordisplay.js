f4d3f8636e6b5ec4bc360d56a52c0c4492853b1b
VinTomie <vincenttomie234@gmail.com>
2018-04-07 21:57:11 -0400

    Update creatordisplay.js

--- public/creatordisplay.js
+++ public/creatordisplay.js
@@ -1,6 +1,6 @@
 exports.list = (req, res) => {
 	console.log("we have activated creatordisplay.js\n");
-	db.query('SELECT * FROM Creators', (error, results, fields) => 
+	db.query('SELECT C.* from Creators C Where C.id not in (select creator from favoriteCreators where user = ?)', [req.session.userid], (error, results, fields) => 
 	{
 		console.log('we did a query');
 		if(error) 
