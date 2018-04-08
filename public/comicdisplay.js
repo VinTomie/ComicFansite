5b5db34a4b89264b4e1743c38f5763fb9e62a738
VinTomie <vincenttomie234@gmail.com>
2018-04-07 21:55:49 -0400

    Update comicdisplay.js

--- public/comicdisplay.js
+++ public/comicdisplay.js
@@ -1,6 +1,6 @@
 exports.list = (req, res) => {
 	console.log("we have activated comicdisplay.js\n");
-	db.query('SELECT * FROM Comics', (error, results, fields) => 
+	db.query('SELECT C.* from Comics C Where C.id not in (select comic from favoriteComics where user = ?)', [req.session.userid], (error, results, fields) => 
 	{
 		console.log('we did a query');
 		if(error) 
